75a06b3d1b1321df5bc2c2e2137eaee3e6a51923
Kunjan <kunjanrana4@gmail.com>
2020-08-17 13:15:51 -0400

    Add files via upload

---
+++ reverse_sb.java
@@ -0,0 +1,13 @@
+//reverse string
+import java.util.*;
+
+class InPlaceReverse
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str = "global",reverse = "";
+		char temp = 'x';
+		StringBuilder sb = new StringBuilder(str);
+		System.out.println("Reverse is "+sb.reverse());
+	}
+}
